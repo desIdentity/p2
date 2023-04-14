206dbee493a285643a001c95eb6f464d809580ac
carmavals <mvcarbajals@pucp.edu.pe>
2023-04-14 01:37:30 -0500

    edicion de velocidad

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let poema = function (r2) {
   let clau2 = ".initClaucanta (es tiempo de introducir “setup” contiene las instrucciones que se ejecutan una sola vez al inicio en cambio “draw” contiene las instrucciones que se ejecutan repetidamente) Dentro de las instrucciones que se ejecutarán al inicio tenemos:“nacer mujer”, ser peruana” y “ser humana” //solo una vez naceré como una ser humana, mujer en Perú //o eso espero //la próxima vez quiero ser una mesa Dentro de “set up” podré que se ejecute una y otra vez sin parar: ser artista “ser artista”//una mesa artista //quiero ser una mesa artista ahora hagamos un par de lineas hay varias en mi “cuerpo” //las “lineas” de mi vida //lineas que marcan mi vida en la mitad de mi abdomen stroke(255,102,0); line(85,20,10,10); line(90,90,15,80); stroke(0,0,0); bezier(85,20,10,10,90,90,15,80); mi cicatriz en mi abdomen está la cicatriz más grande de mi cuerpo line (0,0,CERO); espero que si pongo esto no salga ni una sola linea en mi cuerpo tengo varias lineas y puntos en mi cuerpo algunos ya los acepté //Establecer mi tamaño, el de mi ventana, o sea mi cuerpo anchoCuerpo=900000000 //depende bastante de dónde empieces a medir, mis caderas son lo más ancho altoCuerpo=162 //estoy orgullosa de mi altura??? //Configurar mi cuerpo createCanvas(anchoCuerpo,altoCuerpo)"
   let fer = 'boton = document.getElementById("matan") let mostrarVideo = function(revelar condicionantes y disparadores){console.log("funciona")  algo funciona //si algo funciona entonces algo no funcionaalert("funciona") //impulsivo y tranquilo Las personas con trastorno límite de la personalidad pueden tener cambios fuertes en el estado de ánimo y sentirse inseguras sobre cómo se perciben a sí mismas. Sus sentimientos hacia los demás pueden cambiar rápidamente y pasar de una cercanía extrema a una aversión extrema. //arriesgado} function setup() {createCanvas(900, 400); img = loadImage("./img/fer.png"); colorMode (HSB, 360, 100, 100)} function draw() { //tus dibujos>> quiero ese impulso para hacer trazos que tomen caminos que me despierten algo //Dame ese impulso de vivir  //trazame la distancia hacia a ti background(h, s, x, y); strokeWeight(10); stroke(h, 70, 70); // algo me dice que escuchas a The Strokes //dime tres canciones de The Strokes no me gusta the strokes me gusta gorillaz kanye west mike oldfield'
   let dani = 'int x= mujer muy seguradraw my life(“extremadamente incierta”, 90,90); // ¨or maybe not¨{computarizar mi vida me hace verla con una máscara  neutra en mi rostro}background(19, confused, 2003);stroke(indefinite red); //tryng to mark my boundaries o tratando de definirme ellipse (¿Quien soy?, ¿Que quiero?, ¿A donde voy<´?) DlAiNeI(17, 03, wild, 2003, X); //X= incertidumbre DlAiNeI(IL3ÑA, 7, Mujer, 7, x); //ahora Sani, o sea San Isidro //sintaxis.error IL0 notfound in SaN1 DlAiNeI(0, 400, artista, 400, x, MUJER); DlAiNeI(0, 500, MuJeR Que SiEnt3, 500, x); ...noStroke(X=incertidumbre);{ tal vez la incertidumbre navegó en mi por muchos años} //DlAiNeI son todas las cosas que quiero decir; void(veamoslo): MI niñez (mouseY) //  Y= DESEQUILIBRIO RESTLESS y = map(mouseY, 0, DlAiNeI, 200, mom); //no sé si quiero ser como tu mamá o quizá quiero ser una madre como tú //pero quieres hijxs? draw (MouzeZ)// Z = VANIDAD Adolescence(wildX2, Z, Qu13nS0Y, D4nN4); //no lo sé  //La vanidad me encubrió por mucho tiempo, quise salir. Llamando a  mouseD to other PHASES int D= Despojo de la falsedad para abrazar a la verdad Actress nY = map(DlAiNeI, 0, Si3nt0, D, 100???, R3sp1rar);'
-  let clau = 'ERROR SINTAXIS “este cuerpo debe seguir en pie” //Sigue de pie //Baila sobre el Muy a parte de las cosas físicas están las espirituales void draw (){ sol(escorpio) ascendente(cancer) luna(aries) //mucha agua, tomalo con calma camellos(51) //busqué en google mi valor en camellos, según los estandares de otro continente } Cómo cierras el código de tu cuerpo se cerraría perfecto con mi muerte pero aún no codificaremos eso ... recuerdo que una vez le escribí una carta a un chico y .. significaba un abrazo así que cerremos este código con un abrazo ... initClauabrazo(//aquí vas tú)'
+  //let clau = 'ERROR SINTAXIS “este cuerpo debe seguir en pie” //Sigue de pie //Baila sobre el Muy a parte de las cosas físicas están las espirituales void draw (){ sol(escorpio) ascendente(cancer) luna(aries) //mucha agua, tomalo con calma camellos(51) //busqué en google mi valor en camellos, según los estandares de otro continente } Cómo cierras el código de tu cuerpo se cerraría perfecto con mi muerte pero aún no codificaremos eso ... recuerdo que una vez le escribí una carta a un chico y .. significaba un abrazo así que cerremos este código con un abrazo ... initClauabrazo(//aquí vas tú)'
   let daphne = 'shiftEnter //ctrlShiftEnter ejecuta; shiftEnter qué hace? A veces me miro en el espejo Y no se quien es size(400,400); PImage img = loadImage("hikarudarkhair"); Cuando falleció mi mama, me volví rubia Un tiempo funciono pero luego ya no se quien era size(400,400); PImage img = loadImage("hikarublondehair"); Rubia como bella bella era mi luna en geminis //inserte código astrológico La luna me mandaba mensajes SendmeTexts SendmeTexts //y la dejo en visto, porque ella me dejó en visto primero Luna lunita Déjame salir // vete Déjame vivir // no te vayas Suéltame // vete Suéltame poco a poco // no te vayas Pero no tan fuerte que me caigo'
   let lu = 'String words = "me cifro desde el 0 de mis pies hasta el 1 de mi cabello"; //000.1.01.11. una L minúscula String[] caracter = { "Z", "*", "e", "Mmm", "uwu", "L", "r", "m", "S", "a", "o", "X", "p", "t", "u", "v", "c", “i"}; void setup() { size(153, 153); //se puede? es muy chiquito //en realidad tú eres chiquita, tkm } void draw() { background(90, 99, 100); tendría que ser celeste //¿por el cielo o qué? frameRate(1000/1000, claro que si); stroke(¿????); //suena a smoke sinestesia float a = (TruX / (loextraño //¿huanxaco o malabrigo?) width) * 90f; //llevas dentro a Trujillo, ellxs también te extrañan //los movimientos de ballet no puedo traducirlos al traslado de processing //5ta posicion (05;00) es un problema, pero aki sigo line(4,4,4,1998);  // es mi número favorito, es par, es una mesa translate(0, -999); // me traslado hasta lo más hondo //te trasladas haciendo arte a partir del movimiento branch salsa ranch(26); } void keyTyped() { //The variable "key" always contains the value //dame la llave, ahora if ((key >= "A" && key <= "z") || key == " ") {  letter = key; words = words + key; // las palabras son la llave, usalas }'
  
@@ -24,31 +24,16 @@ let poema = function (r2) {
 r2.draw = function () {
   r2.background(0);
   // Display the credits
-  if(poema == true){
-    for (let i = 0; i < clau.length; i++) {
-      r2.fill(255);
-      r2.text(clau.split(' ')[i], 100, ypos + i * 30)
-      //ypos -= 1;
-    }
   
-    
-    // Move the credits up the screen
-    ypos -= 2;
-    // If the credits have moved off the top of the screen, reset their position
-    if (ypos < -clau.length * 50) {
-      ypos = 600;
-    }
-  }
-
   if(c == true){
     r2.background(255)
     for (let i = 0; i < clau2.length; i++) {
       r2.fill(0);
-      r2.text(username[2], r2.width/2, r2.height/2)
+      r2.text(username[2], r2.width/2.5, r2.height/1.5)
       r2.text(clau2.split(' ')[i], 100, ypos + i * 30)
       
     }   
-  ypos -= 2;
+  ypos -= 0.5;
   if (ypos < -clau2.length * 50) {
     ypos = 600;
   }
@@ -58,12 +43,12 @@ r2.draw = function () {
   if(d == true){
     r2.background(255)
     for (let i = 0; i <dani.length; i++) {
-      r2.text(username[1], r2.width/2, r2.height/2)
+      r2.text(username[1], r2.width/2.5, r2.height/1.5)
       r2.fill(0);
       r2.text(dani.split(' ')[i], 100, ypos + i * 30)
       
     }   
-  ypos -= 2;
+  ypos -= 0.5;
 
   if (ypos < -dani.length * 50) {
     ypos = 600;
@@ -74,12 +59,12 @@ r2.draw = function () {
   if(f == true){
     r2.background(255)
     for (let i = 0; i < fer.length; i++) {
-      r2.text(username[0], r2.width/2, r2.height/2)
+      r2.text(username[0], r2.width/2.5, r2.height/1.5)
       r2.fill(0);
       r2.text(fer.split(' ')[i], 100, ypos + i * 30)
       
     }   
-  ypos -= 2;
+  ypos -= 0.5;
 
   if (ypos < -fer.length * 50) {
     ypos = 600;
@@ -91,12 +76,12 @@ r2.draw = function () {
    r2.background(255)
     for (let i = 0; i < lu.length; i++) {
       
-      r2.text(username[4], r2.width/2, r2.height/2)
+      r2.text(username[4], r2.width/2.5, r2.height/1.5)
       r2.fill(0);
       r2.text(lu.split(' ')[i], 100, ypos + i * 30)
       
     }   
-  ypos -= 2;
+  ypos -= 0.5;
 
   if (ypos < -lu.length * 50) {
     ypos = 600;
@@ -107,12 +92,12 @@ r2.draw = function () {
   if(p == true){
     r2.background(255)
     for (let i = 0; i < daphne.length; i++) {
-      r2.text(username[3], r2.width/2, r2.height/2)
+      r2.text(username[3], r2.width/2.5, r2.height/1.5)
       r2.fill(0);
       r2.text(daphne.split(' ')[i], 100, ypos + i * 30)
       
     }   
-  ypos -= 2;
+  ypos -= 0.5;
   if (ypos < -daphne.length * 50) {
     ypos = 600;
   }
@@ -125,16 +110,7 @@ r2.draw = function () {
 }
 
 r2.keyPressed = function(){
-  if(r2.key == 'm'){
-    l = false
-    c = false
-    d = false
-    p = false
-    f = false
-    b = false
-    poema = true
-    
-  }
+  
 
   if(r2.key == 'c'){
     l = false
